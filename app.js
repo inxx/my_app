62f69d55332bd7578049ece20ed124428783c3c0
inxx <ndur0751@hanmail.net>
2016-07-04 18:59:20 +0900

    app 초기파일 경로 수정

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ var express = require('express');
 
 var app = express();
 
-app.get('/', function(req,res){
-    res.send('hello word!');
-});
+app.use(express.static(__dirname + '/public'));
+//
+//app.get('/', function(req,res){
+//    res.send('hello word!');
+//});
 
 app.listen(3000, function(){
     console.log('Server On!');
