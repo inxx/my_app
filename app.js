25393f879d48c0e2329a6d4077c778ebaca0189d
inxx <inxx@inxxui-iMac.local>
2016-07-21 11:59:24 +0900

    test board

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
+//import modules
 var express = require('express');
 var path = require('path');
 var app = express();
 var mongoose = require('mongoose');
+var badyParser = require('body-parser');
 
-mongoose.connect('mongodb://@ds013574.mlab.com:13574/inxx_data');
+
+//connect db
+mongoose.connect(process.env.m_db);
 
 var db = mongoose.connection;
 
@@ -16,97 +20,51 @@ db.on('error',function(err){
 });
 
 
-var dataSchema = mongoose.Schema({
-  name:String,
-  count:Number
+//model setting
+var postSchema = mongoose.Schema({
+  title : { type:String, required:true},
+  body : { type:String, required:true},
+  createdAt : { type:Date, default:Date.now},
+  updatedAt : Date
 });
+var Post = mongoose.model('post',postSchema);
 
-var Data = mongoose.model('data',dataSchema);
 
-Data.findOne({ name:'myData'}, function(err,data){
-
-  if(err){
-    return console.log('error data :',err);
-  }
-  if(!data){
-    Data.create({name:"myData",count:0}, function(err,data){
-      if(err){
-        return console.log('error data :',err);
-      }
-      console.log('Counter initialized : ',data);
-    });
-  }
-
-});
-
+//view setting
 app.set("view engine", 'ejs');
 
-app.use(express.static(path.join(__dirname, 'public')));
 
+
+//set middlewares
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(badyParser.json());
+
+
+
+//routes setting
 app.get('/', function(req,res){
 
-  Data.findOne({ name:'myData'}, function(err,data){
+  res.render('index_ejs',data);
 
+});
+
+app.get('/posts', function(req,res){
+  Post.find({},function(err,posts){
     if(err){
-      return console.log('error data :',err);
+      return res.json({success:false, message:err});
     }
-    data.count++;
-    data.save(function(err){
-      if(err){
-        return console.log('error data :',err);
-      }
-      res.render('index_ejs',data);
-    });
+    res.json({success:true, data:posts});
   });
-
 });
-
-app.get('/reset', function(req,res){
-  setCounter(res,0);
-});
-
-app.get('/set/count', function(req,res){
-  if(req.query.count){
-      setCounter(res,req.query.count);
-  } else {
-      getCounter(res);
-  }
-});
-
-app.get('/set/:num', function(req,res){
-  if(req.params.num){
-      setCounter(res,req.params.num);
-  } else {
-      getCounter(res);
-  }
-});
-
-
-function setCounter(res,num){
-  console.log('setCounter');
-  Data.findOne({ name:'myData'}, function(err,data){
+app.post('/posts', function(req,res){
+  Post.create(req.body.post, function(err,post){
     if(err){
-      return console.log('error data :',err);
+      return res.json({success:false, message:err});
     }
-    data.count = num;
-    data.save(function(err){
-      if(err){
-        return console.log('error data :',err);
-      }
-      res.render('index_ejs',data);
-    });
+    res.json({success:true, data:post});
   });
-};
+});
 
-function getCounter(res,num){
-  console.log('getCounter');
-  Data.findOne({ name:'myData'}, function(err,data){
-    if(err){
-      return console.log('error data :',err);
-    }
-    res.render('index_ejs',data);
-  });
-};
 
 
 app.listen(3000, function(){
